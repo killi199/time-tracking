08be20d02c8d816b26eab96db68b8f7622389cd9
Kevin Jahrens <killi199@gmx.de>
2026-02-11 19:35:13 +0100

    Improve linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,31 @@ import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import sonarjs from 'eslint-plugin-sonarjs'
+import vitest from '@vitest/eslint-plugin'
 
 export default defineConfig(
     eslint.configs.recommended,
-    tseslint.configs.strict,
+    tseslint.configs.strictTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+    },
     react.configs.flat.recommended,
     react.configs.flat['jsx-runtime'],
     reactHooks.configs.flat.recommended,
     eslintConfigPrettier,
+    sonarjs.configs.recommended,
+    {
+        files: ['**/*.test.ts', '**/*.test.tsx'],
+        plugins: {
+            vitest,
+        },
+        rules: {
+            ...vitest.configs.recommended.rules,
+        },
+    },
 )
